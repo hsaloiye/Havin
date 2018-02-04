c4ea5ea0663d6d0b07c6b9fd464134687f95cc4e
hsaloiye <hsaloiye205@gmail.com>
2018-02-04 14:14:21 -0500

    added test setup - all tests pass now - resolution: remove JUnit4 from buildpath...seems to be a Maven build problem

---
+++ src/test/java/com/boot/TestBase.java
@@ -0,0 +1,14 @@
+package com.boot;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(App.class)
+@ActiveProfiles(profiles = "test")
+public abstract class TestBase {
+
+}
