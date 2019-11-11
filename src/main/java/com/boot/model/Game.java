f069693fc21202318cb173c02bd8fa20845dfc95
hsaloiye <hsaloiye205@gmail.com>
2019-11-11 13:39:43 -0500

    Adding setters/getters to avoid lombok dependency

--- src/main/java/com/boot/model/Game.java
+++ src/main/java/com/boot/model/Game.java
@@ -5,22 +5,82 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 public class Game {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO )
-	public long id;
-	public String name;
-	public String description;
-	public String duration;
-	public int minPlayers;
-	public int maxPlayers;
-	public String genre;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long id;
+    public String name;
+    public String description;
+    public String duration;
+    public int minPlayers;
+    public int maxPlayers;
+    public String genre;
+
+    public Game(long id, String name, String description, String duration, int minPlayers, int maxPlayers, String genre){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.duration = duration;
+        this.minPlayers = minPlayers;
+        this.maxPlayers = maxPlayers;
+        this.genre = genre;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDuration(String duration){
+        this.duration = duration;
+    }
+
+    public String getDuration(){
+        return duration;
+    }
+
+    public void setGenre(String genre){
+        this.genre = genre;
+    }
+
+    public String getGenre(String genre){
+        return genre;
+    }
+
+    public void setMinPlayers(int minPlayers){
+        this.minPlayers = minPlayers;
+    }
+
+    public int getMinPlayers(){
+        return minPlayers;
+    }
+
+    public void setMaxPlayers(int maxPlayers){
+        this.maxPlayers = maxPlayers;
+    }
+
+    public int getMaxPlayers(){
+        return maxPlayers;
+    }
 }
